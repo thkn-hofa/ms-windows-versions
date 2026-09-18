c738bc7d72c2cbfb11e19d9c68c42d6334f21a5f
Thomas Hofkens <thomas.hofkens@savaco.com>
2026-09-18 17:10:23 +0200

    Update links, fix section split, create fallback for wrongly formatted title and update function name for client version parsing

--- Get-WindowsServerBuildsList.ps1
+++ Get-WindowsServerBuildsList.ps1
@@ -1,7 +1,7 @@
 Function Get-WindowsServerBuildsList {
     $WindowsVersionUpdateHistoryURIs = @(
-        "https://support.microsoft.com/en-us/topic/windows-10-and-windows-server-2019-update-history-725fc2e1-4443-6831-a5ca-51ff5cbcb059"
-        "https://support.microsoft.com/en-gb/topic/december-13-2022-kb5021249-os-build-20348-1366-d5fe7608-bc9d-4055-a88c-fb2fd3d5fd45"
+        "https://support.microsoft.com/en-us/servicing/os/windows-10/2022/09/windows-10-update-history"
+        "https://support.microsoft.com/en-gb/servicing/os/windows-server/2024/10/windows-server-2025-update-history"
     )
 
     $Versions = [Hashtable]@{}
@@ -14,7 +14,7 @@ Function Get-WindowsServerBuildsList {
             $UpdateHistoryNavLinks = $NavLinks | ? { $_.outerHTML -match "Update history" }
         }
         Else {
-            $UpdateHistoryNavLinks = $NavLinks | ? { $_.outerHTML -match "Windows Server" } | Select -First 1
+            $UpdateHistoryNavLinks = $NavLinks | ? { $_.outerHTML -match "Windows Server" } | Sort -Unique href
         }
 
         $SectionFirsts = $UpdateHistoryNavLinks | Sort-Object -Unique @{E={([Xml]($_.outerHTML)).a."#text"}},href | Sort-Object @{E={$NavLinks.IndexOf($_)}}
@@ -52,14 +52,35 @@ Function Get-WindowsServerBuildsList {
                     $Versions.Add($VersionNumber, $(New-Object System.Collections.Generic.List[PSCustomObject]))
                 }
 
-                Foreach ($V in ($Sections[$S] | % { ([Xml]($_.outerHTML)).a."#text" })) {
+                # Foreach ($V in ($Sections[$S] | % { ([Xml]($_.outerHTML)).a."#text" })) {
+                Foreach ($Href in $Sections[$S]) {
+                    $V =  ([Xml]($Href.outerHTML)).a."#text"
+
                     # $v
                     $Regex = [Regex]::Matches($V, "(?<date>^[\w\,\s]+).*(?<kb>KB\d+\b).*OS Builds? (?<build>[\d\.]+)")
                     $BuildNumber = ($regex.groups | ? { $_.Name -eq "build" }).Value
                     If (![String]::IsNullOrEmpty($BuildNumber) -and $Versions[$VersionNumber].Build -notcontains $BuildNumber) {
+                        $ParsedDate = [DateTime]::new(0)
+                        $DateValid = [DateTime]::TryParse(($regex.groups | ? { $_.Name -eq "date" }).Value, [ref]$ParsedDate)
+
+                        ## If parsing wasn't valid, open the page to check meta data 'release-date'
+                        If (-not $DateValid) {
+                            Write-Verbose ("Date parsing failed for build {0} with raw date string '{1}'" -f $BuildNumber, ($regex.groups | ? { $_.Name -eq "date" }).Value)
+
+                            $PageURI = [Uri]::new([uri]$URI, $Href.href).AbsoluteUri
+                            
+                            $PageContent = Invoke-WebRequest -usebasicparsing -Uri $PageURI
+                            $PageMetaEntries = [Regex]::Matches($PageContent.Content, "<meta\s+name=['""](?<name>[^'""]+)['""]\s+content=['""](?<content>[^'""]+)['""]\s*/?>", [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
+                            $PageMetaEntriesXML = [Xml]("<root>" + ($PageMetaEntries | ForEach-Object { $_.Value }) + "</root>")
+                            $ReleaseDateMeta = ($PageMetaEntriesXML.root.meta | ? { $_.name -eq "release-date" }).content
+                            If (![String]::IsNullOrEmpty($ReleaseDateMeta)) {
+                                $DateValid = [DateTime]::TryParse($ReleaseDateMeta, [System.Globalization.CultureInfo]::InvariantCulture, [System.Globalization.DateTimeStyles]::None, [ref]$ParsedDate)
+                            }
+                        }
+
                         $Versions[$VersionNumber].Add([PSCustomObject][Ordered]@{
                                 Build = $BuildNumber
-                                Date = ([DateTime]::Parse(($regex.groups | ? { $_.Name -eq "date" }).Value)).ToString("yyyy/MM/dd")
+                                Date = $ParsedDate.ToString("yyyy/MM/dd")
                                 KB = ($regex.groups | ? { $_.Name -eq "kb" }).Value
                                 OutOfBand = $V -match "out\-of\-band"
                                 Preview = $V -match "preview"
